3a14b83a1813a1d283113bed04f8ebea09794192
eric <dpflr0714@gmail.com>
2021-01-25 01:15:31 -0800

    Added keyboard support, not fully functional

--- calculator.js
+++ calculator.js
@@ -1,37 +1,88 @@
 function add(a,b){
-	let result = parseInt(a) + parseInt(b);
+	let result = parseFloat(a) + parseFloat(b);
 	return result;
 }
 
 function subtract(a,b){
-	let result = parseInt(a) - parseInt(b);
+	let result = parseFloat(a) - parseFloat(b);
 	return result;
 }
 
 function multiply(a,b){
-	let result = parseInt(a) * parseInt(b);
+	let result = parseFloat(a) * parseFloat(b);
 	return result;
 }
 
 function divide(a,b){
 	if(b == 0){
-		return "Yooo.. you can't divide by 0"
+		return "Yoo.. you can't divide by 0"
 	} else {
-	let result = parseInt(a) / parseInt(b);
+	let result = parseFloat(a) / parseFloat(b);
 	return result;
 	}
 }
 
 function operator(op, a, b){
-	if(op === "add"){
+	if(op === "+"){
 		return add(a,b)
-	}	else if(op === "subtract"){
+	}	else if(op === "-"){
 		return subtract(a,b)
-	}	else if(op === "multiply"){
+	}	else if(op === "*"){
 		return multiply(a,b)
-	}	else if(op === "divide"){
+	}	else if(op === "/"){
 		return divide(a,b)
 	}
+}
+
+window.addEventListener("keypress", keyboardEvent)
+//window.addEventListener('keydown', backSpace)
+//Array consisting of Unicodes for allowed characters, (#46 is .)  (#13 is =) 
+let allowedNumbers = [48,49,50,51,52,53,54,55,56,57];
+let allowedOperators = [47,42,45,43];
+//need to add backspace support
+function keyboardEvent(){
+	let key = event.which || event.keyCode;
+	if(allowedNumbers.includes(key)){
+		if(clearDisplay === false){
+			display.textContent = "";
+			display.textContent += String.fromCharCode(key);
+			clearDisplay = true;
+		} else if(clearDisplay === true){
+			display.textContent += String.fromCharCode(key);
+		}
+		
+	} else if(allowedOperators.includes(key)){
+		if(operators == undefined){
+			currentNumber = display.textContent;
+			operators = String.fromCharCode(key);
+			if(currentNumber == ""){
+				message.textContent = "Please enter a number first"
+				operators = undefined;
+			}
+		} else {
+			lastNumber = display.textContent;
+			display.textContent = operator(operators, currentNumber, lastNumber);
+			currentNumber = display.textContent;
+			operators = String.fromCharCode(key);
+		}
+
+		clearDisplay = false;
+	} else if(key == "13"){
+
+		if(operators == undefined){
+			null;
+		} else {
+			let lastNumber = display.textContent;
+			display.textContent = operator(operators, currentNumber, lastNumber);
+			operators = undefined;
+			lastNumber = undefined;
+			currentNumber = undefined;
+		}
+		clearDisplay = false;
+	} else if(key == "Backspace"){
+		console.log("Hey")
+	}
+
 }
 
 let runningDisplay = document.getElementById("display");
@@ -40,6 +91,8 @@ let numberInputs = document.getElementsByClassName("numInput");
 let opInputs = document.getElementsByClassName("opInput");
 let equalInput = document.getElementById("equal");
 let allClearInput = document.getElementById("allClear");
+let backSpaceInput = document.getElementById("Backspace");
+let decimalInput = document.getElementById("decimal");
 
 for(let i = 0; i < numberInputs.length; i++){
 	numberInputs[i].addEventListener("click", (event) => {
@@ -81,11 +134,16 @@ for(let i = 0; i < opInputs.length; i++){
 
 equalInput.addEventListener("click", (event) => {
 
+	if(operators == undefined){
+		null;
+	}
+	else {
 	let lastNumber = display.textContent;
 	display.textContent = operator(operators, currentNumber, lastNumber);
 	operators = undefined;
 	lastNumber = undefined;
 	currentNumber = undefined;
+	}
 
 	clearDisplay = false;
 });
@@ -97,3 +155,22 @@ allClearInput.addEventListener("click", (event) => {
 	lastNumber = undefined;
 	currentNumber = undefined;
 });
+
+backSpaceInput.addEventListener("click", (event) => {
+
+	let oldNumber = display.textContent;
+	let newNumber = oldNumber.substring(0, oldNumber.length - 1);
+	display.textContent = newNumber;
+});
+
+decimalInput.addEventListener("click", (event) => {
+
+	searchString = display.textContent;
+
+	if(searchString.includes(".") ==  false){
+		display.textContent += event.target.value;
+	} else {
+		null;
+	}
+});
+
